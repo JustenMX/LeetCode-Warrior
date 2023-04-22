8e15522b79b91072820393eabb6229c8967c715d
Justen Manni <mannijusten@gmail.com>
2023-04-22 15:22:00 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import "./main.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold underline">LeetCode Warrior</h1>
+    </div>
+  );
+}
+
+export default App;
